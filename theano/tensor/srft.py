2df20f4213ecafe1d25a95abfa9e98750fc5d638
Gabriel Krummenacher <gabriel.krummenacher@inf.ethz.ch>
2016-01-11 18:10:23 +0100

    fix srft: make all float32

--- theano/tensor/srft.py
+++ theano/tensor/srft.py
@@ -10,7 +10,7 @@ from theano.gof import Op, Apply
 from theano.gradient import DisconnectedType
 from theano.tensor import basic as tensor
 import numpy as np
-from fjlt.random_projection_fast import fast_unitary_transform_fast_1d
+from fjlt.random_projection_fast import fast_unitary_transform_fast_1d32
 
 logger = logging.getLogger(__name__)
 
@@ -20,17 +20,23 @@ class SRFT(Op):
     """
     __props__ = ('k', 'n')
 
-    def __init__(self, k, n):
+    def __init__(self, k, n):#, wisdom_file='theano_wisdom'):
         self.k = k
         
         self.n = n
-        self.D = np.sign(np.random.randn(self.n))
-        self.srht_const = np.sqrt(self.n / self.k)
+        self.D = np.sign(np.random.randn(self.n)).astype("float32")
+        self.srht_const = np.sqrt(self.n / self.k).astype("float32")
         self.S = np.random.choice(self.n, self.k, replace=False)
+        
+#         self.wisdom_file = wisdom_file
+#         try:
+#             import_wisdom(self.wisdom_file)
+#         except IOError:
+#             print('wisdom file', self.wisdom_file, 'not found, starting new file.')
 
 
     def transform_1d(self, x):
-        a = np.asarray(fast_unitary_transform_fast_1d(x.copy(), D=self.D))
+        a = np.asarray(fast_unitary_transform_fast_1d32(x.copy(), D=self.D))
         return self.srht_const * a[self.S]        
 
     def make_node(self, x):
@@ -47,3 +53,6 @@ class SRFT(Op):
 
         assert x.ndim == 1, "x should be a vector."
         Px[0] = self.transform_1d(x)
+
+#     def __dealloc__(self):
+#         export_wisdom(self.wisdom_file)
